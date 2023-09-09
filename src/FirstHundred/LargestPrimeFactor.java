71a53d3a8f95c376124d9b46e0b2adbc29e5b0dd
Prem Prakash <gmbuddycp@gmail.com>
2023-09-09 14:47:54 +0530

    Largest Prime factor complete !

---
+++ src/FirstHundred/LargestPrimeFactor.java
@@ -0,0 +1,29 @@
+package FirstHundred;
+
+/*
+The prime factors of 13195 are 5,7,13,and 29.
+What is the largest prime factor of the number 600851475143 ?
+*/
+public class LargestPrimeFactor {
+
+// if the no. has square root which not divided by any other no
+    // if it is divided by prime no.
+
+    public static long largestPrimeFactor(long n){
+        int i = 2;
+        while(i != n){
+            if(n%i == 0)
+                n = n/i;
+            else
+                i++;
+        }
+        return n;
+    }
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println(largestPrimeFactor(600851475143l));
+    }
+}
