af1d0cac9c845540b3ffed9a449c9dfa9c7d6750
Prem Prakash <gmbuddycp@gmail.com>
2023-10-16 14:51:32 +0530

    Smallest Multiple complete

--- src/FirstHundred/SmallestMultiple.java
+++ src/FirstHundred/SmallestMultiple.java
@@ -12,43 +12,7 @@ import java.util.List;
 
 public class SmallestMultiple {
 
-
-    //naive approach
-    public static int smallestMultiple(){
-        //finding number which can be divided by all
-        List<Integer> rangeOfNum = new ArrayList<>();
-        for(int i=1;i<=20;i++){
-            rangeOfNum.add(i);
-        }
-        int sm = 1;
-        int num =20;
-
-        for(int i = 20; i <= rangeOfNum.size() && i > 1;i--){
-
-            for (int j=2;j<=rangeOfNum.size()-1 ;j++){
-                System.out.println(j);
-                if(i%rangeOfNum.get(j)==0){
-                    System.out.println(i+""+rangeOfNum.get(j));
-
-//                        rangeOfNum.add(j,0);
-
-
-                }
-            }
-
-        }
-
-        System.out.println(rangeOfNum.toString());
-
-
-        for(int i:rangeOfNum){
-            sm*=i;
-        }
-
-        return sm;
-    }
-
-    public static boolean checkSmallest(int num, int start, int end){
+    public static boolean checkSmallest(long num, int start, int end){
         boolean result = true;
         for (int i=start;i<=end;i++){
             if(num % i !=0){
@@ -58,11 +22,36 @@ public class SmallestMultiple {
         return result;
     }
 
+    // Function to calculate the GCD of two numbers
+    static long gcd(long a, long b) {
+        if (b == 0)
+            return a;
+        return gcd(b, a % b);
+    }
+
+    static long lcm(long a, long b) {
+        return a * (b / gcd(a, b));
+    }
+
+
+    public static long smallestMultiple(List<Long> numbers ){
+        long result = 1;
+        for (long number : numbers) {
+            result = lcm(result, number);
+        }
+        return result;
+
+    }
+
 
 
     public static void main(String[] args) {
 
-        System.out.println(checkSmallest(2520,1,10));
-        System.out.println(smallestMultiple());
+        List<Long> list = new ArrayList<>();
+        for(int i = 1 ; i<= 20 ; i++){
+            list.add((long)i);
+        }
+        System.out.println(checkSmallest(smallestMultiple(list),1,20));
+        System.out.println(smallestMultiple(list));
     }
 }
